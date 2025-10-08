7bd434f9e8fb73c0ba270397a425de21d79c886e
Duz91 <110282202+Duz91@users.noreply.github.com>
2025-10-08 14:53:22 +0200

    WLAN, MQTT init

--- main.py
+++ main.py
@@ -1,45 +1,113 @@
 import utime
 import machine
 import dht
-from machine import Pin 
-from machine import PWM
-from time import sleep
+from machine import Pin, PWM
+import network
+import umqtt.simple as mqtt
 
+# WLAN-Konfiguration
+WLAN_SSID = "dein_wlan_ssid"
+WLAN_PASSWORT = "dein_wlan_passwort"
+
+# MQTT-Konfiguration
+MQTT_BROKER = "dein_mqtt_broker"
+MQTT_CLIENT_ID = "pico_temp_sensor"
+MQTT_TOPIC_TEMP = "temperatur"
+MQTT_TOPIC_HUM = "luftfeuchtigkeit"
+MQTT_TOPIC_DUTY = "duty_cycle"
+MQTT_TOPIC_M = "steigung"
+MQTT_TOPIC_B = "achsenabschnitt"
+
+# Standardwerte für m und b
+m = 4300
+b = -87000
+
+# PWM-Konfiguration
 pwm = PWM(Pin(15))
 pwm.freq(20000)
 led = Pin("LED", Pin.OUT)
 
+# DHT-Konfiguration
+d = dht.DHT22(machine.Pin(28))
+
+# WLAN-Verbindung herstellen
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(WLAN_SSID, WLAN_PASSWORT)
+
+while not wlan.isconnected():
+    print("Verbinde mit WLAN...")
+    utime.sleep(1)
+
+print("WLAN verbunden. IP-Adresse:", wlan.ifconfig()[0])
+
+# MQTT-Client erstellen
+client = mqtt.MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER)
+
+# Callback-Funktion für MQTT-Nachrichten
+def sub_cb(topic, msg):
+    global m, b
+    topic_str = topic.decode('utf-8')
+    msg_str = msg.decode('utf-8')
+    print((topic_str, msg_str))
+    if topic_str == MQTT_TOPIC_M:
+        try:
+            m = float(msg_str)
+            print("Neue Steigung:", m)
+        except ValueError:
+            print("Ungültige Steigung:", msg_str)
+    elif topic_str == MQTT_TOPIC_B:
+        try:
+            b = float(msg_str)
+            print("Neuer Achsenabschnitt:", b)
+        except ValueError:
+            print("Ungültiger Achsenabschnitt:", msg_str)
+
+client.set_callback(sub_cb)
+
+# Mit MQTT-Broker verbinden
+client.connect()
+print("Mit MQTT-Broker verbunden")
+
+# Topics abonnieren
+client.subscribe(MQTT_TOPIC_M)
+client.subscribe(MQTT_TOPIC_B)
+
+# Hauptschleife
 while True:
-    
-    led.on()
-    d = dht.DHT22(machine.Pin(28))
-    d.measure()
-    temp = d.temperature()
-    hum = d.humidity()
-    temp_str = str(temp)
-    hum_str = str(hum)
-    print(temp_str + ' °C')
-    print(hum_str + ' % rel. Luftfeuchte')
-    
-    
-    duty = 4300*temp-87000
-    duty_str = str(duty)
-    
-    print('davor: '+ duty_str)
-    
-    if duty < 0:
-        duty = 0
-    elif duty > 65536:
-        duty = 65536
-        
-    percent = 0.00152587890625*duty
-    percent = int(percent)
-    percent_str = str(percent)
-    print('duty: ' + percent_str + '%')
-    duty_str = str(duty)
-    duty = int(duty)
-    print('danach: ' + duty_str)
-    pwm.duty_u16(duty)
-    led.off()
-    sleep(0.5)
-    
+    try:
+        led.on()
+        d.measure()
+        temp = d.temperature()
+        hum = d.humidity()
+
+        # Berechnung des Duty Cycle mit den aktuellen Werten für m und b
+        duty = int(m * temp + b)
+
+        if duty < 0:
+            duty = 0
+        elif duty > 65535:
+            duty = 65535
+
+        percent = int(0.00152587890625 * duty)
+
+        pwm.duty_u16(duty)
+
+        # Daten an MQTT senden
+        client.publish(MQTT_TOPIC_TEMP, str(temp).encode('utf-8'))
+        client.publish(MQTT_TOPIC_HUM, str(hum).encode('utf-8'))
+        client.publish(MQTT_TOPIC_DUTY, str(percent).encode('utf-8'))
+
+        print("Temperatur:", temp, "°C, Luftfeuchtigkeit:", hum, "%, Duty Cycle:", percent, "%")
+
+        led.off()
+        utime.sleep(5) # Sendet alle 5 Sekunden
+
+        # Auf neue MQTT-Nachrichten prüfen
+        client.check_msg()
+
+    except Exception as e:
+        print("Fehler:", e)
+        client.disconnect()
+        wlan.disconnect()
+        break
